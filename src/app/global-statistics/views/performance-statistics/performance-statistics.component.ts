bd73109c4e3a051b4c471abcd853b5189caa329d
Richard Ivanek <richard.ivanek@gmail.com>
2022-05-18 12:11:20 +0200

    Split Performance > Response

--- src/app/global-statistics/views/performance-statistics/performance-statistics.component.ts
+++ src/app/global-statistics/views/performance-statistics/performance-statistics.component.ts
@@ -1,27 +1,33 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {MetricService, PeriodType} from '../../../metric.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {SystemMetricType} from '../../../common/models/metrics/system-metric-type.enum';
-import {PeriodService} from '../../../period.service';
-import {BusService} from '../../bus.service';
-import {SasiColumnBuilder, SasiSortType, SasiTableOptions} from '../../../common/components/sasi-table/sasi-table.component';
-import {UnitFormatterComponent} from '../../formatters/unit-formatter/unit-formatter.component';
-import {RouteLinkFormatterComponent} from '../../../common/components/route-link-formatter/route-link-formatter.component';
-import {AlertFormatterComponent} from '../../formatters/alert-formatter/alert-formatter.component';
-import {AlertRule, Threshold} from '../../alert-rule';
-import {RowTableComponent} from '../../../common/components/sasi-table/row-table/row-table.component';
-import {SimpleSortImpl} from '../../../common/components/sasi-table/simple-sort-impl';
-import {MetricHandlerUtils} from '../../utils/metric-handler.utils';
-import {StorageEntityMetricDto} from '../../../common/models/dtos/storage-entity-metric.dto';
-import {SeTextFormatterComponent} from '../../../storage-configuration/se-text-formatter/se-text-formatter.component';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { MetricService, PeriodType } from "../../../metric.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { SystemMetricType } from "../../../common/models/metrics/system-metric-type.enum";
+import { PeriodService } from "../../../period.service";
+import { BusService } from "../../bus.service";
+import {
+  SasiColumnBuilder,
+  SasiSortType,
+  SasiTableOptions,
+} from "../../../common/components/sasi-table/sasi-table.component";
+import { UnitFormatterComponent } from "../../formatters/unit-formatter/unit-formatter.component";
+import { RouteLinkFormatterComponent } from "../../../common/components/route-link-formatter/route-link-formatter.component";
+import { AlertFormatterComponent } from "../../formatters/alert-formatter/alert-formatter.component";
+import { AlertRule, Threshold } from "../../alert-rule";
+import { RowTableComponent } from "../../../common/components/sasi-table/row-table/row-table.component";
+import { SimpleSortImpl } from "../../../common/components/sasi-table/simple-sort-impl";
+import { MetricHandlerUtils } from "../../utils/metric-handler.utils";
+import { StorageEntityMetricDto } from "../../../common/models/dtos/storage-entity-metric.dto";
+import { SeTextFormatterComponent } from "../../../storage-configuration/se-text-formatter/se-text-formatter.component";
 
 @Component({
-  selector: 'app-tab',
-  templateUrl: './performance-statistics.component.html',
-  styleUrls: ['./performance-statistics.component.css', '../../global-statistics.component.css'],
+  selector: "app-tab",
+  templateUrl: "./performance-statistics.component.html",
+  styleUrls: [
+    "./performance-statistics.component.css",
+    "../../global-statistics.component.css",
+  ],
 })
 export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
-
   currentPeriod: PeriodType = PeriodType.WEEK;
   data: StorageEntityMetricDto[] = []; // Todo caching data by datacenters
   options: SasiTableOptions = new SasiTableOptions();
@@ -36,8 +42,8 @@ export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
   ) {
     this.options.columns.push(
       SasiColumnBuilder.getInstance()
-        .withIndex('name')
-        .withLabel('System')
+        .withIndex("name")
+        .withLabel("System")
         .withComponent(RouteLinkFormatterComponent)
         .withAltSortEnable(false)
         .withIsAggregated(false)
@@ -46,9 +52,11 @@ export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
     this.options.columns.push(
       SasiColumnBuilder.getInstance()
         .withIndex(SystemMetricType.WORKLOAD)
-        .withLabel('Workload')
-        .withTooltipText('Workload Average')
-        .withColumnTooltipText('Average/(Peak) value of transactions per second (IOPS) for selected time period (Last Day / Last Week / Last Month)')
+        .withLabel("Workload")
+        .withTooltipText("Workload Average")
+        .withColumnTooltipText(
+          "Average/(Peak) value of transactions per second (IOPS) for selected time period (Last Day / Last Week / Last Month)"
+        )
         .withComponent(UnitFormatterComponent)
         .withAltSortEnable(true)
         .withIsAggregated(false)
@@ -57,9 +65,11 @@ export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
     this.options.columns.push(
       SasiColumnBuilder.getInstance()
         .withIndex(SystemMetricType.TRANSFER)
-        .withLabel('Transfer')
-        .withTooltipText('Transfer Average')
-        .withColumnTooltipText('Average/(Peak) value of transferred data blocks per second (MB/s) for selected time period (Last Day / Last Week / Last Month)')
+        .withLabel("Transfer")
+        .withTooltipText("Transfer Average")
+        .withColumnTooltipText(
+          "Average/(Peak) value of transferred data blocks per second (MB/s) for selected time period (Last Day / Last Week / Last Month)"
+        )
         .withComponent(UnitFormatterComponent)
         .withAltSortEnable(true)
         .withIsAggregated(false)
@@ -68,9 +78,24 @@ export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
     this.options.columns.push(
       SasiColumnBuilder.getInstance()
         .withIndex(SystemMetricType.RESPONSE)
-        .withLabel('Response')
-        .withTooltipText('Response Average')
-        .withColumnTooltipText('Average/(Peak) value of latency on all LDEVs (both - read and write) for selected time period (Last Day / Last Week / Last Month)')
+        .withLabel("Read Response")
+        .withTooltipText("Read Response Average")
+        .withColumnTooltipText(
+          "Average/(Peak) value of read latency on all LDEVs (both - read and write) for selected time period (Last Day / Last Week / Last Month)"
+        )
+        .withComponent(UnitFormatterComponent)
+        .withAltSortEnable(true)
+        .withIsAggregated(false)
+        .build()
+    );
+    this.options.columns.push(
+      SasiColumnBuilder.getInstance()
+        .withIndex(SystemMetricType.RESPONSE_WRITE)
+        .withLabel("Write Response")
+        .withTooltipText("Write Response Average")
+        .withColumnTooltipText(
+          "Average/(Peak) value of write latency on all LDEVs (both - read and write) for selected time period (Last Day / Last Week / Last Month)"
+        )
         .withComponent(UnitFormatterComponent)
         .withAltSortEnable(true)
         .withIsAggregated(false)
@@ -79,9 +104,11 @@ export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
     this.options.columns.push(
       SasiColumnBuilder.getInstance()
         .withIndex(SystemMetricType.CPU)
-        .withLabel('CPU')
-        .withTooltipText('CPU Average')
-        .withColumnTooltipText('Average/(Peak) value of utilization on all system processors (MPBs) for selected time period (Last Day / Last Week / Last Month)')
+        .withLabel("CPU")
+        .withTooltipText("CPU Average")
+        .withColumnTooltipText(
+          "Average/(Peak) value of utilization on all system processors (MPBs) for selected time period (Last Day / Last Week / Last Month)"
+        )
         .withComponent(UnitFormatterComponent)
         .withAltSortEnable(true)
         .withIsAggregated(false)
@@ -90,9 +117,11 @@ export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
     this.options.columns.push(
       SasiColumnBuilder.getInstance()
         .withIndex(SystemMetricType.HDD)
-        .withLabel('HDD')
-        .withTooltipText('HDD Average')
-        .withColumnTooltipText('Average/(Peak) value of utilization on all hard disk drives / FMDs / SSD used in DP Pools for selected time period (Last Day / Last Week / Last Month)')
+        .withLabel("HDD")
+        .withTooltipText("HDD Average")
+        .withColumnTooltipText(
+          "Average/(Peak) value of utilization on all hard disk drives / FMDs / SSD used in DP Pools for selected time period (Last Day / Last Week / Last Month)"
+        )
         .withComponent(UnitFormatterComponent)
         .withAltSortEnable(true)
         .withIsAggregated(false)
@@ -101,9 +130,11 @@ export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
     this.options.columns.push(
       SasiColumnBuilder.getInstance()
         .withIndex(SystemMetricType.WRITE_PENDING_PERC)
-        .withLabel('Write Pending')
-        .withTooltipText('Write Pending Average')
-        .withColumnTooltipText('Average/(Peak) value of Cache Write Pending rate on all cache modules for selected time period (Last Day / Last Week / Last Month)')
+        .withLabel("Write Pending")
+        .withTooltipText("Write Pending Average")
+        .withColumnTooltipText(
+          "Average/(Peak) value of Cache Write Pending rate on all cache modules for selected time period (Last Day / Last Week / Last Month)"
+        )
         .withComponent(UnitFormatterComponent)
         .withAltSortEnable(true)
         .withIsAggregated(false)
@@ -111,44 +142,49 @@ export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
     );
     this.options.columns.push(
       SasiColumnBuilder.getInstance()
-        .withIndex('sortId')
-        .withLabel('Sort ID')
+        .withIndex("sortId")
+        .withLabel("Sort ID")
         .withComponent(SeTextFormatterComponent)
         .withAltSortEnable(false)
         .withHidden(true)
         .build()
     );
     this.options.rowComponentFormatter = RowTableComponent;
-    this.options.labelColumnWidth = '13.78';
-    this.options.valueColumnWidth = '13.78';
+    this.options.labelColumnWidth = "13.78";
+    this.options.valueColumnWidth = "13.78";
     this.options.sortService = new SimpleSortImpl();
-    this.options.altSortColumnName = 'peak';
-    this.options.sortColumnNames = ['sortId', 'name'];
+    this.options.altSortColumnName = "peak";
+    this.options.sortColumnNames = ["sortId", "name"];
     this.options.sortType = SasiSortType.ASC;
-
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(
-      params => {
-        const id = +params.get('id');
-        this.bus.announceDatacenter(id);
-        this.bus.announceContext('performance');
-        this.data = this.getTableData(id);
+    this.route.paramMap.subscribe((params) => {
+      const id = +params.get("id");
+      this.bus.announceDatacenter(id);
+      this.bus.announceContext("performance");
+      this.data = this.getTableData(id);
+    });
+    this.periodService.periodAnnouncement$.subscribe((period) => {
+      if (this.currentPeriod !== period) {
+        this.currentPeriod = period;
+        this.getTableData(this.currentDataCenterId);
       }
-    );
-    this.periodService.periodAnnouncement$.subscribe(
-      period => {
-        if (this.currentPeriod !== period) {
-          this.currentPeriod = period;
-          this.getTableData(this.currentDataCenterId);
-        }
-      }
-    );
+    });
     this.periodService.announceEnablePeriod(true);
     this.periodService.announcePeriod(this.currentPeriod);
-    this.options.cellDecoratorRules.push(new AlertRule(SystemMetricType.CPU, new Threshold('text-orange', 80, 10000)));
-    this.options.cellDecoratorRules.push(new AlertRule(SystemMetricType.WRITE_PENDING_PERC, new Threshold('text-orange', 30, 10000)));
+    this.options.cellDecoratorRules.push(
+      new AlertRule(
+        SystemMetricType.CPU,
+        new Threshold("text-orange", 80, 10000)
+      )
+    );
+    this.options.cellDecoratorRules.push(
+      new AlertRule(
+        SystemMetricType.WRITE_PENDING_PERC,
+        new Threshold("text-orange", 30, 10000)
+      )
+    );
   }
 
   ngOnDestroy(): void {
@@ -157,10 +193,12 @@ export class PerformanceStatisticsComponent implements OnInit, OnDestroy {
 
   getTableData(id: number): StorageEntityMetricDto[] {
     this.currentDataCenterId = id;
-    this.metricService.getPerformanceStatistics(id, this.currentPeriod).subscribe(
-      data => this.data = MetricHandlerUtils.success(data),
-      error => this.data = MetricHandlerUtils.error(error)
-    );
+    this.metricService
+      .getPerformanceStatistics(id, this.currentPeriod)
+      .subscribe(
+        (data) => (this.data = MetricHandlerUtils.success(data)),
+        (error) => (this.data = MetricHandlerUtils.error(error))
+      );
     return this.data;
   }
 }
