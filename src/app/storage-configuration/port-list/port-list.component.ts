a85055f72b5bc9ae753319d3e4dde527d48640d5
Richard Ivánek <richard.ivanek@gmail.com>
2022-08-14 21:49:01 +0200

    Three new port columns

--- src/app/storage-configuration/port-list/port-list.component.ts
+++ src/app/storage-configuration/port-list/port-list.component.ts
@@ -1,127 +1,64 @@
-import {Component} from '@angular/core';
-import {StorageEntityType} from '../../common/models/dtos/owner.dto';
-import {SasiColumnBuilder} from '../../common/components/sasi-table/sasi-table.component';
-import {MetricService} from '../../metric.service';
-import {FormBusService} from '../form-bus.service';
-import {SeTextFormatterComponent} from '../se-text-formatter/se-text-formatter.component';
-import {RowTableComponent} from '../../common/components/sasi-table/row-table/row-table.component';
-import {SimpleSortImpl} from '../../common/components/sasi-table/simple-sort-impl';
-import {StorageEntityList} from '../channel-board-list/channel-board-list.component';
-import {SpeedFormatterComponent} from '../speed-formatter/speed-formatter.component';
-import {StorageEntityStatusComponent} from '../storage-entity-status/storage-entity-status.component';
+import { Component, Type } from "@angular/core";
+import { StorageEntityType } from "../../common/models/dtos/owner.dto";
+import { SasiColumnBuilder } from "../../common/components/sasi-table/sasi-table.component";
+import { MetricService } from "../../metric.service";
+import { FormBusService } from "../form-bus.service";
+import { SeTextFormatterComponent } from "../se-text-formatter/se-text-formatter.component";
+import { RowTableComponent } from "../../common/components/sasi-table/row-table/row-table.component";
+import { SimpleSortImpl } from "../../common/components/sasi-table/simple-sort-impl";
+import { StorageEntityList } from "../channel-board-list/channel-board-list.component";
+import { SpeedFormatterComponent } from "../speed-formatter/speed-formatter.component";
+import { StorageEntityStatusComponent } from "../storage-entity-status/storage-entity-status.component";
 
 @Component({
-  selector: 'app-port-list',
-  templateUrl: '../channel-board-list/channel-board-list.component.html',
-  styleUrls: ['../channel-board-list/channel-board-list.component.css']
+  selector: "app-port-list",
+  templateUrl: "../channel-board-list/channel-board-list.component.html",
+  styleUrls: ["../channel-board-list/channel-board-list.component.css"],
 })
 export class PortListComponent extends StorageEntityList {
-
-  constructor(protected metricService: MetricService,
-              protected formBus: FormBusService) {
+  constructor(
+    protected metricService: MetricService,
+    protected formBus: FormBusService
+  ) {
     super(metricService, formBus, StorageEntityType.PORT);
   }
 
   ngOnInit() {
-    this.options.columns.push(
-      SasiColumnBuilder.getInstance()
-        .withIndex('parentName')
-        .withLabel('Channel Board')
-        .withComponent(SeTextFormatterComponent)
-        .withAltSortEnable(false)
-        .withIsAggregated(false)
-        .build()
-    );
-    this.options.columns.push(
-      SasiColumnBuilder.getInstance()
-        .withIndex('name')
-        .withAltLabel('Name')
-        .withLabel('Name')
-        .withComponent(SeTextFormatterComponent)
-        .withAltSortEnable(false)
-        .withIsAggregated(false)
-        .build()
-    );
+    const cols: [string, string, Type<any> | undefined][] = [
+      ["parentName", "Channel Board", undefined],
+      ["name", "Name", undefined],
+      ["speed", "Speed", SpeedFormatterComponent],
+      ["cables", "Cables", undefined],
+      ["switch", "Switch", undefined],
+      ["coversRoom", "Covers the room", undefined],
+      ["automation", "Automation", undefined],
+      ["throughput", "Throughput", SpeedFormatterComponent],
+      ["slot", "Slot/Port", undefined],
+      ["note", "Description", undefined],
+      ["wwn", "WWN", undefined],
+      ["status", "Active", StorageEntityStatusComponent],
+    ];
 
-    this.options.columns.push(
-      SasiColumnBuilder.getInstance()
-        .withIndex('speed')
-        .withAltLabel('Speed')
-        .withLabel('Speed')
-        .withComponent(SpeedFormatterComponent)
-        .withAltSortEnable(false)
-        .withIsAggregated(false)
-        .build()
-    );
-
-    this.options.columns.push(
-      SasiColumnBuilder.getInstance()
-        .withIndex('cables')
-        .withAltLabel('Cables')
-        .withLabel('Cables')
-        .withComponent(SeTextFormatterComponent)
-        .withAltSortEnable(false)
-        .withIsAggregated(false)
-        .build()
-    );
-    this.options.columns.push(
-      SasiColumnBuilder.getInstance()
-        .withIndex('switch')
-        .withAltLabel('Switch')
-        .withLabel('Switch')
-        .withComponent(SeTextFormatterComponent)
-        .withAltSortEnable(false)
-        .withIsAggregated(false)
-        .build()
-    );
-    this.options.columns.push(
-      SasiColumnBuilder.getInstance()
-        .withIndex('slot')
-        .withAltLabel('Slot/Port')
-        .withLabel('Slot/Port')
-        .withComponent(SeTextFormatterComponent)
-        .withAltSortEnable(false)
-        .withIsAggregated(false)
-        .build()
-    );
-    this.options.columns.push(
-      SasiColumnBuilder.getInstance()
-        .withIndex('note')
-        .withAltLabel('Description')
-        .withLabel('Description')
-        .withComponent(SeTextFormatterComponent)
-        .withAltSortEnable(false)
-        .withIsAggregated(false)
-        .build()
-    );
-    this.options.columns.push(
-      SasiColumnBuilder.getInstance()
-        .withIndex('wwn')
-        .withAltLabel('WWN')
-        .withLabel('WWN')
-        .withComponent(SeTextFormatterComponent)
-        .withAltSortEnable(false)
-        .withIsAggregated(false)
-        .build()
-    );
-    this.options.columns.push(
-      SasiColumnBuilder.getInstance()
-        .withIndex('status')
-        .withLabel('Active')
-        .withComponent(StorageEntityStatusComponent)
-        .withAltSortEnable(false)
-        .build()
-    );
+    for (const [idx, label, formatter] of cols) {
+      this.options.columns.push(
+        SasiColumnBuilder.getInstance()
+          .withIndex(idx)
+          .withLabel(label)
+          .withAltLabel(label)
+          .withComponent(formatter ?? SeTextFormatterComponent)
+          .build()
+      );
+    }
 
     this.options.rowComponentFormatter = RowTableComponent;
     // this.options.grIndexComponentFormatter = SpeedFormatterComponent;
     this.options.isDataGrouped = false;
     this.options.selectableRows = true;
     this.options.storeSelectedRows = false;
-    this.options.storageNamePrefix = 'portList';
+    this.options.storageNamePrefix = "portList";
     this.options.highlightRow = true;
     this.options.highlightColumn = false;
     this.options.sortService = new SimpleSortImpl();
-    this.options.sortColumnNames = ['name'];
+    this.options.sortColumnNames = ["name"];
   }
 }
--- src/app/storage-configuration/storage-entity-status/storage-entity-status.component.ts
+++ src/app/storage-configuration/storage-entity-status/storage-entity-status.component.ts
@@ -1,54 +1,51 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {SystemMetric} from '../../common/models/metrics/system-metric.vo';
-import {SasiColumn, SasiRow} from '../../common/components/sasi-table/sasi-table.component';
-import {FormBusService} from '../form-bus.service';
-import {StorageEntityVo} from '../storage-entity-form/storage-entity-form.component';
-import {StorageEntityType} from '../../common/models/dtos/owner.dto';
-import {ComponentStatus} from '../../common/models/dtos/enums/component.status';
+import { Component, Input, OnInit } from "@angular/core";
+import { SystemMetric } from "../../common/models/metrics/system-metric.vo";
+import {
+  SasiColumn,
+  SasiRow,
+} from "../../common/components/sasi-table/sasi-table.component";
+import { FormBusService } from "../form-bus.service";
+import { StorageEntityVo } from "../storage-entity-form/storage-entity-form.component";
+import { StorageEntityType } from "../../common/models/dtos/owner.dto";
+import { ComponentStatus } from "../../common/models/dtos/enums/component.status";
 
 @Component({
-  selector: 'app-storage-entity-status',
-  templateUrl: './storage-entity-status.component.html',
-  styleUrls: ['./storage-entity-status.component.css']
+  selector: "app-storage-entity-status",
+  templateUrl: "./storage-entity-status.component.html",
+  styleUrls: ["./storage-entity-status.component.css"],
 })
 export class StorageEntityStatusComponent implements OnInit {
-
   @Input() label;
   @Input() public data: SystemMetric;
   @Input() public column: SasiColumn;
   @Input() public rowData: SasiRow;
 
-  constructor(private formBus: FormBusService) {
-  }
+  constructor(private formBus: FormBusService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   openForm() {
     const formData = new StorageEntityVo();
     formData.type = StorageEntityType.SYSTEM;
-    formData.status = this.getCellValue('status');
-    formData.serialNumber = this.getCellValue('serialNumber');
-    formData.parentId = this.getCellValue('parentId');
-    formData.prefixReferenceId = this.getCellValue('prefixReferenceId');
-    formData.name = this.getCellValue('name');
-    formData.id = this.getCellValue('id');
-    formData.dkc = this.getCellValue('dkc');
-    formData.room = this.getCellValue('room');
-    formData.rack = this.getCellValue('rack');
-    formData.arrayModel = this.getCellValue('arrayModel');
-    formData.managementIp = this.getCellValue('managementIp');
-    this.formBus.sendFormData({data: formData, selectedData: []});
+    formData.status = this.getCellValue("status");
+    formData.serialNumber = this.getCellValue("serialNumber");
+    formData.parentId = this.getCellValue("parentId");
+    formData.prefixReferenceId = this.getCellValue("prefixReferenceId");
+    formData.name = this.getCellValue("name");
+    formData.id = this.getCellValue("id");
+    formData.dkc = this.getCellValue("dkc");
+    formData.room = this.getCellValue("room");
+    formData.rack = this.getCellValue("rack");
+    formData.arrayModel = this.getCellValue("arrayModel");
+    formData.managementIp = this.getCellValue("managementIp");
+    this.formBus.sendFormData({ data: formData, selectedData: [] });
   }
 
   isActive() {
-    return this.getCellValue('status') === 1;
+    return this.getCellValue("status") === 1;
   }
 
   getCellValue(valueName: string) {
-    if (this.rowData.cells[valueName] !== undefined) {
-      return this.rowData.cells[valueName].value;
-    }
-    return null;
+    return this.rowData.cells[valueName].value;
   }
 }
